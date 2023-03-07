91d94a220a65953661e75c0cb7afe271adf82937
Gildardo <gil@im.unam.mx>
2023-03-07 08:36:46 -0600

    update to 4.3.20

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,6 +2,6 @@
 
 # see https://community.plone.org/t/not-using-bootstrap-py-as-default/620
 rm -r ./lib ./include ./bin
-virtualenv-2.7 --no-setuptools --clear .
-./bin/pip install -r requirements.txt
-./bin/buildout
+pyenv local migration2.7
+pip install -r requirements.txt
+buildout
